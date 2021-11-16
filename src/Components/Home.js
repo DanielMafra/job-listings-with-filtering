e8eafea2d0f3cd2901dbbbe537532f32935cfc47
Daniel Mafra <danielmafradev@gmail.com>
2021-11-16 09:25:32 -0300

    First commit

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+const Home = () => {
+
+  return (
+    <div>
+      Job listings with filtering
+    </div>
+  );
+};
+
+export default Home;
